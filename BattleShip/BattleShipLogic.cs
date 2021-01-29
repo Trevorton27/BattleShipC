90f067683ad33c750449e4e0f3bbb4a0d2473c95
Trevor Mearns <spiral272@gmail.com>
2021-01-29 13:39:13 +0900

    Add project files.

---
+++ BattleShip/BattleShipLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class BattleShipLogic
+    {
+        public int HitCount { get; set; }
+        public int X_axis { get; set; }
+        public int Y_axis { get; set; }
+
+   
+        public BattleShipLogic()
+        {
+            SetPosition();
+            Console.WriteLine();
+        }
+        public void SetPosition()
+        {
+            var placeBattleShip = new Random();
+            X_axis = placeBattleShip.Next(1, 11);
+            Y_axis = placeBattleShip.Next(1, 11);
+        }
+
+     
+    }
+
+  
+}
---
+++ BattleShip/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BattleShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var battleShipLogic = new BattleShipLogic();
+            Console.WriteLine(battleShipLogic.SetPostion());
+        }
+    }
+}
